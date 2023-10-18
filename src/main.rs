581c1187d94a1ff945ffe2168a8e4b2d82c17432
Braden Everson <bradeneverson@gmail.com>
2023-10-18 06:57:51 -0500

    Updated readme

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use triton::network::{network::Network, activations};
+use triton_grow::network::{network::Network, activations};
 
 fn main() {
     let mut new_net: Network = Network::new(vec![1,2,3,4,2], activations::SIGMOID, 0.1);
