cb16fd606711dd4e4c61fd099fee285f76d80aa8
Braden Everson <bradeneverson@gmail.com>
2023-10-24 05:42:42 -0500

    Added new test case for Rosie

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ use triton_grow::network::{network::Network, activations, modes::Mode};
 fn main() {
     let mut inputs = vec![vec![0.0,0.0],vec![1.0,0.0],vec![0.0,1.0],vec![1.0,1.0]];
     let mut outputs = vec![vec![0.0],vec![1.0],vec![1.0],vec![0.0]];
-    let mut new_net: Network = Network::new(vec![2,3,1], activations::SIGMOID, 0.1);
+    let mut new_net: Network = Network::new(vec![2,3,4,1], activations::SIGMOID, 0.1);
     
     new_net = new_net.train_to_loss(inputs, outputs, 0.001, 10000, Mode::Avg, 0.9, 10.0, 3, 10);
-    println!("1 and 0: {:?}", new_net.feed_forward(&vec![1.0,0.0]));
+    println!("1 and 0: {:?}", new_net.feed_forward(&vec![1.0,0.0])[0].round());
+    println!("0 and 1: {:?}", new_net.feed_forward(&vec![0.0,1.0])[0].round());
+    println!("1 and 1: {:?}", new_net.feed_forward(&vec![1.0,1.0])[0].round());
+    println!("0 and 0: {:?}", new_net.feed_forward(&vec![0.0,0.0])[0].round());
     println!("Net network made: {:?}", new_net.layers);
 
 }
