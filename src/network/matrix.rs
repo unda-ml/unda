062a4e03d1cc666112b4740f9ab132c232f69e07
Braden Everson <bradeneverson@gmail.com>
2023-11-05 19:35:56 -0600

    Added strassen's matrix multiplication algorithm and made it parral;el :)

--- src/network/matrix.rs
+++ src/network/matrix.rs
@@ -55,22 +55,148 @@ impl ops::Mul<&Matrix> for Matrix{
         if self.columns != other.rows{
             panic!("Matrix multiplication is in invalid format");
         }
-
-        let mut res = Matrix::new_empty(self.rows, other.columns);
-        res.data.iter_mut().enumerate().for_each(|(i, row)| {
-            row.iter_mut().enumerate().for_each(|(j, cell)| {
-                *cell = self.data[i]
-                    .iter()
-                    .zip(other.data.iter().map(|r| &r[j]))
-                    .map(|(a, b)| a * b)
-                    .sum();
-            });
-        });
-        res
+        self.par_multiply(other)
     }
 }
 
 impl Matrix{
+
+    fn par_multiply(&self, other: &Matrix) -> Matrix {
+        assert_eq!(self.columns, other.rows);
+
+        if self.rows <= 64 || self.columns <= 64 || other.columns <= 64 {
+            // If the matrices are small enough, use the standard algorithm
+            return self.standard_multiply(other);
+        }
+
+        // Split matrices into quadrants
+        let a11 = self.submatrix(0, 0, self.rows / 2, self.columns / 2);
+        let a12 = self.submatrix(0, self.columns / 2, self.rows / 2, self.columns / 2);
+        let a21 = self.submatrix(self.rows / 2, 0, self.rows / 2, self.columns / 2);
+        let a22 = self.submatrix(self.rows / 2, self.columns / 2, self.rows / 2, self.columns / 2);
+
+        let b11 = other.submatrix(0, 0, other.rows / 2, other.columns / 2);
+        let b12 = other.submatrix(0, other.columns / 2, other.rows / 2, other.columns / 2);
+        let b21 = other.submatrix(other.rows / 2, 0, other.rows / 2, other.columns / 2);
+        let b22 = other.submatrix(other.rows / 2, other.columns / 2, other.rows / 2, other.columns / 2);
+
+        // Calculate 7 products recursively in parallel
+        let p1 = rayon::join(|| a11.multiply(&(b12.clone() - &b22)), || a11.multiply(&(b12.clone() - &b22)));
+        let p2 = rayon::join(|| (a11.clone() + &a12).multiply(&b22), || (a11.clone() + &a12).multiply(&b22));
+        let p3 = rayon::join(|| (a21.clone() + &a22).multiply(&b11), || (a21.clone() + &a22).multiply(&b11));
+        let p4 = rayon::join(|| a22.multiply(&(b21.clone() - &b11)), || a22.multiply(&(b21.clone() - &b11)));
+        let p5 = rayon::join(
+            || (a11.clone() + &a22).multiply(&(b11.clone() + &b22)),
+            || (a11.clone() + &a22).multiply(&(b11.clone() + &b22)),
+        );
+        let p6 = rayon::join(
+            || (a12.clone() - &a22).multiply(&(b21.clone() + &b22)),
+            || (a12.clone() - &a22).multiply(&(b21.clone() + &b22)),
+        );
+        let p7 = rayon::join(
+            || (a11.clone() - &a21).multiply(&(b11.clone() + &b12)),
+            || (a11.clone() - &a21).multiply(&(b11.clone() + &b12)),
+        );
+
+        // Calculate result matrices
+        let c11 = rayon::join(
+            || p5.0.clone() + &p4.0 - &p2.0 + &p6.0,
+            || p5.1.clone() + &p4.1 - &p2.1 + &p6.1,
+        );
+        let c12 = rayon::join(|| p1.0.clone() + &p2.0, || p1.1.clone() + &p2.1);
+        let c21 = rayon::join(|| p3.0.clone() + &p4.0, || p3.1.clone() + &p4.1);
+        let c22 = rayon::join(
+            || p5.0.clone() + &p1.0 - &p3.0 - &p7.0,
+            || p5.1.clone() + &p1.1 - &p3.1 - &p7.1,
+        );
+
+        // Combine the quadrants into the result matrix
+        let mut result = Matrix::new_empty(self.rows, other.columns);
+        result.set_submatrix(0, 0, &c11.0);
+        result.set_submatrix(0, other.columns / 2, &c12.0);
+        result.set_submatrix(self.rows / 2, 0, &c21.0);
+        result.set_submatrix(self.rows / 2, other.columns / 2, &c22.0);
+
+        result
+    }
+
+    fn multiply(&self, other: &Matrix) -> Matrix {
+        assert_eq!(self.columns, other.rows);
+
+        if self.rows <= 64 || self.columns <= 64 || other.columns <= 64 {
+            // If the matrices are small enough, use the standard algorithm
+            return self.standard_multiply(other);
+        }
+
+        // Split matrices into quadrants
+        let a11 = self.submatrix(0, 0, self.rows / 2, self.columns / 2);
+        let a12 = self.submatrix(0, self.columns / 2, self.rows / 2, self.columns / 2);
+        let a21 = self.submatrix(self.rows / 2, 0, self.rows / 2, self.columns / 2);
+        let a22 = self.submatrix(self.rows / 2, self.columns / 2, self.rows / 2, self.columns / 2);
+
+        let b11 = other.submatrix(0, 0, other.rows / 2, other.columns / 2);
+        let b12 = other.submatrix(0, other.columns / 2, other.rows / 2, other.columns / 2);
+        let b21 = other.submatrix(other.rows / 2, 0, other.rows / 2, other.columns / 2);
+        let b22 = other.submatrix(other.rows / 2, other.columns / 2, other.rows / 2, other.columns / 2);
+
+        // Calculate 7 products recursively
+        let p1 = a11.multiply(&(b12.clone() - &b22));
+        let p2 = (a11.clone() + &a12).multiply(&b22);
+        let p3 = (a21.clone() + &a22).multiply(&b11);
+        let p4 = a22.multiply(&(b21.clone() - &b11));
+        let p5 = (a11.clone() + &a22).multiply(&(b11.clone() + &b22));
+        let p6 = (a12.clone() - &a22).multiply(&(b21.clone() + &b22));
+        let p7 = (a11.clone() - &a21).multiply(&(b11.clone() + &b12));
+
+        // Calculate result matrices
+        let c11 = p5.clone() + &p4 - &p2 + &p6;
+        let c12 = p1.clone() + &p2;
+        let c21 = p3.clone() + &p4;
+        let c22 = p5.clone() + &p1 - &p3 - &p7;
+
+        // Combine the quadrants into the result matrix
+        let mut result = Matrix::new_empty(self.rows, other.columns);
+        result.set_submatrix(0, 0, &c11);
+        result.set_submatrix(0, other.columns / 2, &c12);
+        result.set_submatrix(self.rows / 2, 0, &c21);
+        result.set_submatrix(self.rows / 2, other.columns / 2, &c22);
+
+        result
+    }
+
+    fn submatrix(&self, row_start: usize, col_start: usize, rows: usize, cols: usize) -> Matrix {
+        let mut submatrix = Matrix::new_empty(rows, cols);
+        for i in 0..rows {
+            for j in 0..cols {
+                submatrix.data[i][j] = self.data[row_start + i][col_start + j];
+            }
+        }
+        submatrix
+    }
+
+    fn set_submatrix(&mut self, row_start: usize, col_start: usize, other: &Matrix) {
+        for i in 0..other.rows {
+            for j in 0..other.columns {
+                self.data[row_start + i][col_start + j] = other.data[i][j];
+            }
+        }
+    }
+
+    fn standard_multiply(&self, other: &Matrix) -> Matrix {
+        assert_eq!(self.columns, other.rows);
+
+        let mut result = Matrix::new_empty(self.rows, other.columns);
+
+        for i in 0..self.rows {
+            for j in 0..other.columns {
+                for k in 0..self.columns {
+                    result.data[i][j] += self.data[i][k] * other.data[k][j];
+                }
+            }
+        }
+
+        result
+    }
 
     fn new_empty(rows: usize, cols: usize) -> Matrix{
         Matrix{
