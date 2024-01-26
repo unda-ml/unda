91629559c8c376db7b6251b2a798eb373c22b7fd
Braden Everson <bradeneverson@gmail.com>
2024-01-26 17:43:08 -0600

    Made distributions all adhere to a normal map

--- src/network/layer/distributions.rs
+++ src/network/layer/distributions.rs
@@ -14,11 +14,14 @@ pub enum Distributions{
 impl Distributions {
     pub fn sample(&self, rng: &mut Box<dyn RngCore>) -> f32 {
         let mut res = 0.0;
+        let normal: Normal<f32> = Normal::new(0.0, 1.0).unwrap();
         while res.is_zero() {
             res = match self {
-                Distributions::Xavier(layer_size_prev, layer_size_curr) => rng.gen_range(-10.0..10.0) * get_xavier_range(*layer_size_prev, *layer_size_curr),
+                Distributions::Xavier(layer_size_prev, layer_size_curr) => {
+                    rng.sample(normal) * 10.0 * get_xavier_range(*layer_size_prev, *layer_size_curr)
+                },
                 Distributions::He(layer_size_before) => { 
-                    rng.gen_range(-10.0..10.0) * get_he_range(*layer_size_before)
+                    rng.sample(normal) * 10.0 * get_he_range(*layer_size_before)
                 }
                 Distributions::Default => rng.gen_range(-10.0..10.0),
                 Distributions::Ranged(range) => rng.gen_range(range.clone())
