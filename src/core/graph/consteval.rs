c9a3cca6bb67d11752ab6078f4ba3cf9e7648806
Braden Everson <bradeneverson@gmail.com>
2024-02-27 06:22:09 -0600

    Stubbed out new traversal method that looks nicer

--- src/core/graph/consteval.rs
+++ src/core/graph/consteval.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use xla::Literal;
 
 use super::*;
@@ -15,62 +17,45 @@ impl Context {
         if modification_limit == 0 {
             return Ok(true);
         }
-        // TODO: implement this
-        let input_node = &mut self.nodes[input.into()];
-        return match input_node.operation {
-            Operation::Add(a, b) => {
-                let node_a = &self.nodes[a];
-                let node_b = &self.nodes[b];
-                
 
-                if node_a.is_zero()? || node_b.is_zero()? {
+        let mut modifications: usize = 0;
+        let mut changed = false;
 
-                    let a_val = node_a.is_const();
-                    let b_val = node_b.is_const();
-                    
-                    let new_val: Literal;
+        let mut to_visit = vec![input.into()];
+        let mut visitied: HashSet<NodeIdentifier> = HashSet::new();
 
-                    if b_val.is_some() && node_b.is_zero()? {
-
-                    } else {
-
-                    }
+        while let Some(node_id) = to_visit.pop() {
+            if visitied.contains(&node_id.into()) || modifications >= modification_limit {
+                continue;
+            }
+            
+            if let Some(node) = self.nodes.get(node_id) {
+                match node.operation {
+                    Operation::Add(a, b) | Operation::Mul(a, b) => {
+                        if let (Some(a_node), Some(b_node)) = (self.nodes.get(a.into()), self.nodes.get(b.into())) {
+                            
+                            //TODO, if add and one of the nodes is zero convert current node to be
+                            //just the node that isn't zero
+                            //
+                            //If mul and one of the nodes is zero, zero out the new node
+                            //If mul and one of the nodes is one, set the node to just be
+                            //the non-one node
+                            //
+                            //Increment modifications only if these cases are met
+                            modifications += 1;
+                            //Enqueue the dependent nodes to check both of them for constant
+                            //mul/adding
+                            to_visit.push(a.into());
+                            to_visit.push(b.into());
+                        }
+                    },
+                    _ => {},
                 }
- 
-                //TODO at some point possible, for now we're only checkng 
-                //for mul by zero/one and adding by 0
-                /*if a_const.is_some() && b_const.is_some() {
-                    let (a,b) = (a_const.unwrap(), b_const.unwrap());
-                    //TODO: Do replacement
-                    
-                }*/
-                Ok(false)
             }
-            Operation::Mul(a, b) => {
-                let node_a = &self.nodes[a];
-                let node_b = &self.nodes[b];
+            
+            visitied.insert(node_id.into());
+        }
 
-                let a_const = node_a.is_const();
-                let b_const = node_b.is_const();
-
-                if a_const.is_some() || b_const.is_some() {
-                    //TODO: Do replacement
-                    
-                } else if node_a.is_zero()? || node_b.is_zero()? {
-                    //TODO: x * 0 situation, make it zero
-                    
-                }
-
-                Ok(false)
-            }
-            _ =>
-            //TODO: Not fully sure if const folding needs to happen when the
-            //operation isn't addition or multiplication, returnign false
-            //if the operation isn't either of these for now, but definitely
-            //let me know if this should be other behavior
-            {
-                Ok(false)
-            }
-        };
+        Ok(changed)
     }
 }
