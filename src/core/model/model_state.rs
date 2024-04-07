a725d3dcd0f0f83ec1ce238109253ced91843b7c
Braden Everson <bradeneverson@gmail.com>
2024-04-07 16:44:33 -0500

    Dense layers for model

--- src/core/model/model_state.rs
+++ src/core/model/model_state.rs
@@ -1,9 +1,14 @@
-use crate::core::{graph::Context, neural_net::prelude::initializers::Initializers};
+use crate::core::{graph::{Context, Result, NodeIdentifier}, neural_net::prelude::initializers::Initializers};
+
+use super::model_builder::ModelBuilder;
 
 #[allow(dead_code)]
 pub struct Model{
     model_ctx: Context,
-    initializer: Initializers
+    initializer: Initializers,
+
+    curr_node: Option<NodeIdentifier>,
+    weight_bias_pairs: Vec<(NodeIdentifier, NodeIdentifier)>
 }
 
 impl Default for Model {
@@ -16,7 +21,9 @@ impl Model {
     pub fn new() -> Self {
         Self { 
             model_ctx: Context::new(),
-            initializer: Initializers::Default
+            initializer: Initializers::Default,
+            curr_node: None,
+            weight_bias_pairs: vec![]
         }
     }
     pub fn set_initializer(&mut self, new_init: Initializers) {
@@ -24,5 +31,19 @@ impl Model {
     }
     pub fn compile(&mut self) -> Self {
         todo!();
+    }
+    pub fn dense(&mut self, out_size: u32, name: &str) -> Result<()> {
+        if let Some(node) = self.curr_node {
+            //Append dense layer onto end of current context
+            let (out, (weights_curr, bias_curr)) = ModelBuilder::dense(&mut self.model_ctx, node, out_size, name)?;
+            self.curr_node = Some(out);
+            self.weight_bias_pairs.push((weights_curr, bias_curr));
+
+        } else {
+            //Create initial dense layer
+            todo!();
+        }
+
+        Ok(())
     }
 }
