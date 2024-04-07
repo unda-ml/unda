e746f1ffb63c2e9f67d56b2f4a0138121eadea80
Braden Everson <bradeneverson@gmail.com>
2024-04-07 14:48:59 -0500

    Impl default because clippy likes that

--- src/core/model/model.rs
+++ src/core/model/model.rs
@@ -4,6 +4,12 @@ pub struct Model{
     model_ctx: Context
 }
 
+impl Default for Model {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl Model {
     pub fn new() -> Self {
         todo!();
