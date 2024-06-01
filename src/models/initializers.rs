3f5986063e35e515ff0f46d6b09bd493624ca96b
Eben Kadile <eben.kadiler24@gmail.com>
2024-06-01 12:04:10 +0200

    working on initialization

--- src/models/initializers.rs
+++ src/models/initializers.rs
@@ -1,5 +1,18 @@
 use crate::graph::shape::Shape;
 
 pub trait Initializer {
-    fn initialize(seed: i64, shape: Shape) -> xla::Literal;
+    fn initialize(&self, seed: i64, shape: &Shape, dtype: xla::ElementType) -> xla::Result<xla::Literal>;
+}
+
+struct Zeroes {
+}
+
+impl Initializer for Zeroes {
+    fn initialize(&self, seed: i64, shape: &Shape, dtype: xla::ElementType) -> xla::Result<xla::Literal> {
+        let zeroes_vec = [0i64].repeat(shape.size());
+        let zeroes_r1 = xla::Literal::vec1(&zeroes_vec);
+        let shape_i64 = shape.sizes.iter().map(|x| *x as i64).collect::<Vec<i64>>();
+        let zeroes_shaped = xla::Literal::reshape(&zeroes_r1, &shape_i64)?;
+        xla::Literal::convert(&zeroes_shaped, dtype.primitive_type())
+    }
 }
--- src/models/layers.rs
+++ src/models/layers.rs
@@ -30,24 +30,6 @@ impl<T> From<Vec<T>> for ConvParams<T> {
     }
 }
 
-/*
-impl<T> Tree<T, ()> for ConvParams<T> {
-    fn flatten(self) -> (Vec<Box<T>>, ()) {
-        (vec![self.kernel, self.bias], ())
-    }
-    fn unflatten(flat: &mut Vec<Box<T>>, _: ()) -> Self {
-        let kernel = match flat.pop() {
-            None => panic!("Tried to unflatten empty vector into ConvParams!"),
-            Some(w) => w,
-        };
-        let bias = match flat.pop() {
-            None => panic!("Tried to unflatten vector of length 1 in to ConvParams"),
-            Some(b) => b,
-        };
-        ConvParams { kernel, bias }
-    }
-}
-*/
 impl Context {
     pub fn dense<IW: Initializer, IB: Initializer>(
         &mut self,
@@ -55,10 +37,10 @@ impl Context {
         out_size: u32,
         kernel_initializer: IW,
         bias_initializer: IB,
+        kernel_seed: i64,
+        bias_seed: i64,
         name: &str,
     ) -> Result<(NodeIdentifier, ConvParams<NodeIdentifier>, ConvParams<xla::Literal>)> {
-        panic!("Not implemented!")
-        /*
         let shape = self.nodes[input_node].shape.clone();
         let last_dim = shape.sizes[shape.ndims() - 1];
         let dtype = self.nodes[input_node].dtype;
@@ -66,12 +48,8 @@ impl Context {
         let kernel_shape = Shape::from([last_dim, out_size]);
         let mut kernel_name = name.to_owned();
         kernel_name.push_str("_kernel");
-        let kernel = self.parameter(kernel_name, kernel_shape, dtype)?;
-        let kernel_init = initializer.initialize(
-            self,
-            kernel,
-            self.nodes[input_node].shape.sizes[1] as usize,
-        )?;
+        let kernel_val = kernel_initializer.initialize(kernel_seed, &kernel_shape, dtype)?;
+        let kernel_id = self.parameter(kernel_name, kernel_shape, dtype)?;
 
         let mut bias_shape = Shape::new();
         for _ in 0..(shape.ndims() - 1) {
@@ -80,12 +58,12 @@ impl Context {
         bias_shape.sizes.push(out_size);
         let mut bias_name = name.to_owned();
         bias_name.push_str("_bias");
-        let bias = self.parameter(bias_name, bias_shape, dtype)?;
+        let bias_val = bias_initializer.initialize(bias_seed, &bias_shape, dtype)?;
+        let bias_id = self.parameter(bias_name, bias_shape, dtype)?;
 
-        let matmul_node = self.matmul(input_node, kernel_init)?;
-        let dense_node = self.add(matmul_node, bias)?;
+        let matmul_node = self.matmul(input_node, kernel_id)?;
+        let dense_node = self.add(matmul_node, bias_id)?;
 
-        Ok((dense_node, (kernel_init, bias)))
-        */
+        Ok((dense_node, ConvParams{kernel: kernel_id, bias: bias_id}, ConvParams{kernel: kernel_val, bias: bias_val}))
     }
 }
